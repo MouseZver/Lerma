daa8d5b150948dfbcac64e145c8ff56de9419c84
Mouse <mouse-zver@xaker.ru>
2017-12-10 18:13:37 +0300

    Лерма

---
+++ Aero/Database/Migrate.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Aero\Database;
+
+use Aero;
+use Exception;
+use Throwable;
+
+class Migrate
+{
+	private static $instance;
+	protected $migrate;
+	protected $driver;
+	
+	/*
+		- Выбор и загрузка драйвера для работы с базой данных
+	*/
+	protected function IDrivers( string $name ): Migrate
+	{
+		$this -> migrate = ( $Lerma = new $name() ) -> migrate;
+		
+		$driverPath = 'driver' . DIRECTORY_SEPARATOR . $Lerma -> driver . '.php';
+		
+		if ( !file_exists ( __DIR__ . DIRECTORY_SEPARATOR . $driverPath ) )
+		{
+			throw new Exception( 'Драйвер Лермы не найден. ' . $driverPath );
+		}
+		
+		$this -> driver = require $driverPath;
+		
+		return $this;
+	}
+	
+	/*
+		- Запуск ядра
+	*/
+	protected static function instance(): Migrate
+	{
+		if ( self::$instance === NULL )
+		{
+			self::$instance = ( new static ) -> IDrivers( Aero\Configures\Lerma::class );
+		}
+		
+		return self::$instance;
+	}
+	
+	/*
+		- Определение запроса на форматирование строки
+	*/
+	protected static function query( $sql )
+	{
+		return ( $static = self::instance() ) -> driver -> query( is_array ( $sql ) ? sprintf ( ...$sql ) : $sql );
+	}
+	
+	/*
+		- Определение подготовленного запроса на форматирование строки
+	*/
+	protected static function prepare( $sql, array $execute = [] )
+	{
+		if ( empty ( $execute ) )
+		{
+			throw new Exception( 'Значения для подготовления пусты' );
+		}
+		
+		( $static = self::instance() ) -> driver -> prepare( is_array ( $sql ) ? sprintf ( ...$sql ) : $sql );
+		
+		if ( $static -> isMulti( $execute ) )
+		{
+			try
+			{
+				$static -> driver -> begin_transaction();
+				
+				$static -> multiExecute( $execute );
+				
+				$static -> driver -> commit();
+			}
+			catch ( Throwable $Throwable )
+			{
+				$static -> driver -> rollback();
+				
+				exit ( $Throwable -> getMessage() );
+			}
+		}
+		else
+		{
+			$static -> driver -> execute( $execute );
+		}
+		
+		return $static -> driver;
+	}
+	
+	/*
+		- Проверяем данные на мульти-запрос
+	*/
+	protected function isMulti( array $array )
+	{
+		if ( is_array ( current ( $array ) ) )
+		{
+			foreach ( $array AS $items )
+			{
+				if ( !is_array ( $items ) )
+				{
+					throw new Exception( 'Ошибка в мульти добавлении, запрос не выполнен. Ожидался полный многомерный массив.' );
+				}
+			}
+			
+			return TRUE;
+		}
+		else
+		{
+			foreach ( $array AS $items )
+			{
+				if ( is_array ( $items ) )
+				{
+					throw new Exception( 'Ошибка в добавлении, запрос не выполнен. Ожидался не многомерный массив.' );
+				}
+			}
+			
+			return FALSE;
+		}
+	}
+	
+	/*
+		- Многократное добавление данных подготовленного запроса в бд
+	*/
+	protected function multiExecute( array $executes )
+	{
+		
+		foreach ( $executes AS $execute )
+		{
+			self::$instance -> driver -> execute( $execute );
+		}
+		commit();
+	}
+}
---
+++ test/bootstrap.php
@@ -0,0 +1,54 @@
+<?php
+
+use Aero\Supports\Lerma;
+
+require '../autoload.php';
+
+# ------------------------
+
+/* 
+fetch()
+	FETCH_NUM
+	FETCH_ASSOC
+	FETCH_OBJ
+	FETCH_BIND
+	FETCH_BIND | FETCH_COLUMN
+	FETCH_COLUMN
+	FETCH_KEY_PAIR
+	FETCH_FUNC
+	FETCH_CLASS
+	FETCH_CLASSTYPE
+
+fetchAll()
+	FETCH_NUM
+	FETCH_ASSOC
+	FETCH_OBJ
+	FETCH_COLUMN
+	FETCH_KEY_PAIR
+	FETCH_KEY_PAIR | FETCH_NAMED
+	FETCH_UNIQUE
+	FETCH_GROUP
+	FETCH_GROUP | FETCH_COLUMN
+	FETCH_FUNC
+	FETCH_CLASS
+	FETCH_CLASSTYPE
+	Lerma::FETCH_CLASSTYPE | Lerma::FETCH_UNIQUE
+*/
+
+# ------------------------
+
+$table = 'lerma';
+
+$sql = [ 'SELECT * FROM %s', $table ]; # or 'SELECT * FROM lerma'
+
+$query = Lerma::query( $sql ) -> fetchAll( Lerma::FETCH_OBJ );
+
+# ------------------------
+
+$sql = [ [ 'SELECT * FROM %s WHERE id IN ( ?,?,? )', 'lerma' ], [ 3,9,81 ] ];
+
+$prepare = Lerma::prepare( ...$sql ) -> fetchAll( Lerma::FETCH_OBJ );
+
+# ------------------------
+
+print_r ( compact ( 'query', 'prepare' ) );
