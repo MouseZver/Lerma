cc28fc732cf3b35c798d135cdb7fcbf30813f942
MouseZver <mouse-zver@xaker.ru>
2022-02-23 11:17:16 +0300

    replace *_all

--- src/LermaInterface.php
+++ src/LermaInterface.php
@@ -22,36 +22,36 @@ interface LermaInterface
 	public const MODE = [
 		Lerma :: FETCH_NUM => [
 			'fetch_num', 
-			'all' => 'fetchall_num' 
+			'all' => 'fetch_num_all' 
 		],
 		Lerma :: FETCH_ASSOC => [
 			'fetch_assoc', 
-			'all' => 'fetchall_assoc' 
+			'all' => 'fetch_assoc_all' 
 		],
 		Lerma :: FETCH_OBJ => [
 			'fetch_obj', 
-			'all' => 'fetchall_obj' 
+			'all' => 'fetch_obj_all' 
 		],
 		Lerma :: FETCH_COLUMN => [ 
 			'fetch_column', 
-			'all' => 'fetchall_obj' 
+			'all' => 'fetch_obj_all' 
 		],
 		Lerma :: FETCH_FUNC => [ 
 			'fetch_func', 
-			'all' => 'fetchall_obj' 
+			'all' => 'fetch_obj_all' 
 		],
 		Lerma :: FETCH_KEY_PAIR => [ 
 			'fetch_key_pair', 
-			'all' => 'fetchall_key_pair' 
+			'all' => 'fetch_key_pair_all' 
 		],
 		//Lerma :: FETCH_KEY_PAIR | Lerma :: FETCH_NAMED => [ 'all' => 'fetchall_key_pair' ],
-		Lerma :: FETCH_KEY_PAIR | Lerma :: FETCH_FUNC => [ 'all' => 'fetchall_key_pair' ],
-		Lerma :: FETCH_UNIQUE => [ 'all' => 'fetchall_unique' ],
-		Lerma :: FETCH_GROUP => [ 'all' => 'fetchall_group' ],
-		Lerma :: FETCH_GROUP | Lerma :: FETCH_COLUMN => [ 'all' => 'fetchall_group_column' ],
+		Lerma :: FETCH_KEY_PAIR | Lerma :: FETCH_FUNC => [ 'all' => 'fetch_key_pair_all' ],
+		Lerma :: FETCH_UNIQUE => [ 'all' => 'fetch_unique_all' ],
+		Lerma :: FETCH_GROUP => [ 'all' => 'fetch_group_all' ],
+		Lerma :: FETCH_GROUP | Lerma :: FETCH_COLUMN => [ 'all' => 'fetch_group_column_all' ],
 		Lerma :: MYSQL_FETCH_FIELD => [ 
 			'fetch_field', 
-			'all' => 'fetchall_field' 
+			'all' => 'fetch_field_all' 
 		],
 		Lerma :: MYSQL_FETCH_BIND => [ 'fetch_bind' ],
 		Lerma :: MYSQL_FETCH_BIND | Lerma :: FETCH_COLUMN => [ 'fetch_bind' ],
